0412263a6560535a877bc2e3fbb38c6bdf5f654e
Roman <rominator835@gmail.com>
2021-10-03 18:04:14 +0200

    lol

--- src/main/java/tbu/blockmod/entities/EntityRegister.java
+++ src/main/java/tbu/blockmod/entities/EntityRegister.java
@@ -1,18 +1,14 @@
 package tbu.blockmod.entities;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.lwjgl.system.CallbackI;
 import tbu.blockmod.entities.alexentitypackage.AlexEntity;
-import tbu.blockmod.entities.romanentitypackage.RomanEntity;
 
 public class EntityRegister implements ModInitializer {
 
@@ -22,18 +18,9 @@ public class EntityRegister implements ModInitializer {
             new Identifier("blockmod", "alex"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE , AlexEntity::new).dimensions(EntityDimensions.fixed(1.0f, 2.0f)).build()
     );
-
-    public static final EntityType<RomanEntity> ROMAN = Registry.register(
-            Registry.ENTITY_TYPE,
-            new Identifier("blockmod", "roman"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, RomanEntity::new).dimensions(EntityDimensions.fixed(1.0f,2.0f)).build()
-    );
-
-
     @Override
     public void onInitialize() {
 
         FabricDefaultAttributeRegistry.register(ALEX, AlexEntity.createMobAttributes());
-        FabricDefaultAttributeRegistry.register(ROMAN, RomanEntity.createPiglinAttributes());
     }
 }
