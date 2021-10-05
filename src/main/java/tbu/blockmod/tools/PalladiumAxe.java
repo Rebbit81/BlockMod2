cadbd9796e56e5452a27240c337459df1d038a3b
Roman <rominator835@gmail.com>
2021-10-05 17:36:38 +0200

    lol

---
+++ src/main/java/tbu/blockmod/tools/PalladiumAxe.java
@@ -0,0 +1,12 @@
+package tbu.blockmod.tools;
+
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class PalladiumAxe extends AxeItem {
+    public PalladiumAxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
+
+
