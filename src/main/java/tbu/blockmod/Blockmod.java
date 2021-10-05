462b7c150ad6a7d3213c289c01ca4029a6f9f842
Alex <alex@epostnet.de>
2021-10-05 18:21:39 +0200

    Texturen

--- src/main/java/tbu/blockmod/Blockmod.java
+++ src/main/java/tbu/blockmod/Blockmod.java
@@ -2,58 +2,21 @@ package tbu.blockmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tbu.blockmod.blocks.ModBlocks;
 import tbu.blockmod.events.LootTableCallbacks;
 import tbu.blockmod.items.ModItems;
 import tbu.blockmod.worldgen.PalladiumEndOreGen;
 
 public class Blockmod implements ModInitializer {
-
-
-    public static String createItemModelJson(String id, String type) {
-        if ("generated".equals(type) || "handheld".equals(type)) {
-            //The two types of items. "handheld" is used mostly for tools and the like, while "generated" is used for everything else.
-            return "{\n" +
-                    "  \"parent\": \"item/" + type + "\",\n" +
-                    "  \"textures\": {\n" +
-                    "    \"layer0\": \"blockmod:item/" + id + "\"\n" +
-                    "  }\n" +
-                    "}";
-        } else if ("block".equals(type)) {
-            //However, if the item is a block-item, it will have a different model json than the previous two.
-            return "{\n" +
-                    "  \"parent\": \"blockmod:block/" + id + "\"\n" +
-                    "}";
-        }
-        else {
-            //If the type is invalid, return an empty json string.
-            return "";
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public static final ItemGroup BM_GROUP = FabricItemGroupBuilder.create(
@@ -65,5 +28,6 @@ public class Blockmod implements ModInitializer {
         ModBlocks.init();
         PalladiumEndOreGen.init();
         LootTableCallbacks.init();
+
     }
 }
