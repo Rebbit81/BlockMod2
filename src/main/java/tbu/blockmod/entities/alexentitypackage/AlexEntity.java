d2de626cec385dfbbd2fd49ee5e9d9645e5e9e0a
Alex <alex@epostnet.de>
2021-10-06 18:26:10 +0200

    Texturen

--- src/main/java/tbu/blockmod/entities/alexentitypackage/AlexEntity.java
+++ src/main/java/tbu/blockmod/entities/alexentitypackage/AlexEntity.java
@@ -3,6 +3,8 @@ package tbu.blockmod.entities.alexentitypackage;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.world.World;
 
 public class AlexEntity extends PathAwareEntity {
