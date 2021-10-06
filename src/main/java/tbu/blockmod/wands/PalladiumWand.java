8b5d0ffc5c1d5b3f3a800d0258e419eb87243aa2
Alex <alex@epostnet.de>
2021-10-06 17:27:26 +0200

    Texturen

--- src/main/java/tbu/blockmod/wands/PalladiumWand.java
+++ src/main/java/tbu/blockmod/wands/PalladiumWand.java
@@ -1,12 +1,12 @@
 package tbu.blockmod.wands;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class PalladiumWand extends Item {
@@ -15,9 +15,21 @@ public class PalladiumWand extends Item {
         super(settings);
     }
 
+
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
 
-            FireballEntity fireballEntity = new FireballEntity(playerEntity.world, playerEntity, 5, 5, 5, 5);
+
+             Vec3d vec3d = playerEntity.getEyePos();
+             double d = playerEntity.getEyePos().length();
+             vec3d = playerEntity.getEyePos().normalize();
+
+            double e = 4.0D;
+            Vec3d vec3dd = playerEntity.getRotationVec(1.0F);
+            double f = playerEntity.getX() - (playerEntity.getX() + vec3dd.x * 4.0D);
+            double g = playerEntity.getBodyY(0.5D) - (0.5D + playerEntity.getBodyY(0.5D));
+            double h = playerEntity.getZ() - (playerEntity.getZ() + vec3dd.z * 4.0D);
+
+            FireballEntity fireballEntity = new FireballEntity(playerEntity.world, playerEntity, d, e, f, 5);
             fireballEntity.setPosition(playerEntity.getX(), playerEntity.getBodyY(0.5D) + 0.5D, fireballEntity.getZ());
             world.spawnEntity(fireballEntity);
 
@@ -25,5 +37,5 @@ public class PalladiumWand extends Item {
             return TypedActionResult.success(playerEntity.getStackInHand(hand));
         }
 
+    }
 
-}
